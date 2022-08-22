e18a6300f2b6d27c9632590ac4aba11c7a7ce47e
Christian Beckham <urenacb22@gmail.com>
2022-08-22 16:19:27 -0500

    feat: added function to build a two dimensional list for storing each player's hand

--- main.py
+++ main.py
@@ -13,15 +13,37 @@ def generate_deck():
 
 
 def shuffle_deck(deck):
+    temp_deck = deck[:]
 
-    for index in range(len(deck)):
-        random_number = randrange(0, len(deck))
-        card = deck.pop(index)
-        deck.insert(random_number, card)
+    for index in range(len(temp_deck)):
+        random_number = randrange(0, len(temp_deck))
+        card = temp_deck.pop(index)
+        temp_deck.insert(random_number, card)
 
-    return deck
+    return temp_deck
 
 
-deck = generate_deck()
-shuffled_deck = shuffle_deck(deck)
-print('Shuffled deck of cards', shuffled_deck)
+def deal_cards(shuffled_deck, num_of_players):
+    all_hands = []
+
+    for _ in range(num_of_players):
+        hand = []
+
+        for index in range(0, 5):
+            hand.append(shuffled_deck.pop(index))
+
+        all_hands += [hand]
+    
+    return all_hands
+
+
+def start_game(number_of_players, rounds_to_play):
+
+    deck = generate_deck()
+    shuffled_deck = shuffle_deck(deck)
+    hands = deal_cards(shuffled_deck, number_of_players)
+    for key, value in enumerate(hands):
+        print(f'Player {key + 1} has a hand of {value}')
+
+
+start_game(4, 1)
