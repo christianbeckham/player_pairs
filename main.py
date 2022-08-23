d5b84da9369a2b0105ec1fc8a3623de93edb6b74
Christian Beckham <urenacb22@gmail.com>
2022-08-23 13:44:25 -0500

    feat: improved start game function to take in user input and improved game logic

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ def calculate_pairs(cards):
     return total_pairs
 
 
-def deal_cards(shuffled_deck, num_of_players):
+def deal_cards(shuffled_deck, num_of_players, cards_per_player):
     all_hands = []
 
     for index in range(num_of_players):
@@ -45,7 +45,7 @@ def deal_cards(shuffled_deck, num_of_players):
             'pairs': 0
         }
 
-        for index in range(0, 5):
+        for index in range(0, cards_per_player):
             card = shuffled_deck.pop(0)
 
             if card not in hand['cards']:
@@ -94,16 +94,38 @@ def calculate_game_result(players):
             f"\nThere is a tie among {players_minus_last} and {last_player}.")
 
 
-def start_game(number_of_players, rounds_to_play):
+def start_game():
     print('\nWelcome to Player Pairs!')
-    print('In this game each player will receive 5 cards.\nOnce each hand has been dealt, we will compare to see who has the most pairs!')
+    print('In this game each player will receive a hand of cards.\nOnce each hand has been dealt, we will compare to see who has the most pairs!')
 
-    deck = generate_deck()
-    shuffled_deck = shuffle_deck(deck)
-    hands = deal_cards(shuffled_deck, number_of_players)
-    display_player_hands(hands)
-    sorted_players_by_pairs = sort_by_highest_pairs(hands)
-    calculate_game_result(sorted_players_by_pairs)
+    number_of_players = int(input('\nEnter the number of players: '))
+    rounds_to_play = int(input('Enter the number of rounds to play: '))
+
+    cards_per_player = 5
+    cards_per_game = number_of_players * cards_per_player
+
+    valid_game = True if cards_per_game <= 52 else False
+
+    if valid_game:
+        original_deck = generate_deck()
+        shuffled_deck = shuffle_deck(original_deck)
+
+        for round in range(1, rounds_to_play + 1):
+            print('\nRound:', round)
+
+            if len(shuffled_deck) <= cards_per_game:
+                shuffled_deck = shuffle_deck(original_deck)
+
+            hands = deal_cards(
+                shuffled_deck, number_of_players, cards_per_player)
+            display_player_hands(hands)
+            sorted_players_by_pairs = sort_by_highest_pairs(hands)
+            calculate_game_result(sorted_players_by_pairs)
+    else:
+        print('Invalid game: Too many players.')
+        return
+
+    print('\nGAME OVER.')
 
 
-start_game(4, 1)
+start_game()
